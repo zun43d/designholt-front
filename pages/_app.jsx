4e44e327732fe3575649f8bf7cd19b1b5a18b09c
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-01-19 02:14:07 +0600

    Checkout page scratched!

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import { addPaypalScript } from '@/utils/addPaypalScript';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { CartProvider } from 'react-use-cart';
 import AuthProvider from '@/context/AuthUserContext';
@@ -5,6 +8,12 @@ import { theme } from '@/styles/theme';
 import '@fontsource/inter/variable-full.css';
 
 function MyApp({ Component, pageProps }) {
+	const [paypalLoaded, setPaypalLoaded] = useState(false);
+	// useEffect(() => {
+	// 	addPaypalScript(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, setPaypalLoaded);
+	// 	console.log(paypalLoaded);
+	// }, [paypalLoaded]);
+
 	return (
 		<AuthProvider>
 			<ChakraProvider theme={theme}>
