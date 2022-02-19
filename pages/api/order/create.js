f2a5a76e41a166ac821d3cf2cf795cbfd68ed6b1
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-02-20 03:03:18 +0600

    Checkout page API issue fixed

--- pages/api/order/create.js
+++ pages/api/order/create.js
@@ -1,90 +1,5 @@
-import {
-	createBuyer,
-	updateBoughtItems,
-	productsAndVendors,
-} from '@/lib/sanityAdmin';
-import sgEmail from '@sendgrid/mail';
-
-sgEmail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const sendProductOnEmail = async (items, name, email) => {
-	try {
-		const msg = {
-			to: email,
-			from: 'funnyclub2015@gmail.com',
-			subject: 'Your download is ready',
-			// text: 'sdasdawd',
-			html: `
-        <p>Hi ${name},</p>
-        <p>Thank you for purchasing "Logo Template <a href="https://designholt.com/logo-license">(Logo Stock License)</a>" from Designholt. Read the license 
-        properly before using the "Logo Template". You can download the "Logo Template" using the "Download" button below. 
-        The product will be available on this link for the next 30 days. That's why we recommend saving the logo in secure 
-        storage.</p>
-        <a href="https://designholt.com/logo-license">Read the license here</a>
-        <br />
-        <br />
-        <table style="
-          font-family: arial, sans-serif;
-          border-collapse: collapse;
-          width: 90%;
-          margin: auto;
-        ">
-          <tr>
-            <th style="
-              border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-            ">Product</th>
-            <th style="
-              border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-            ">Price (USD)</th> 
-            <th style="
-              border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-            ">Action</th>
-          </tr>
-          ${items
-						.map(
-							(item) => `
-            <tr>
-              <td style="
-                border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-              ">${item.title}</td>
-              <td style="
-                border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-              ">${item.price}$</td>
-              <td style="
-                border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-              "><a href="https://designholt.com/download?productId=${
-								item.id
-							}&userId=${
-								's5d1a4sd56aw' /* userid goes here */
-							}">Download</a></td>
-            </tr>
-          `
-						)
-						.join('')}
-        </table>
-      `,
-		};
-
-		return await sgEmail
-			.send(msg)
-			.then((data) => data)
-			.catch((err) => console.log(err));
-	} catch (err) {
-		console.log('sendProductEmail\n', err);
-	}
-};
+import axios from 'axios';
+import { createBuyer, updateBoughtItems } from '@/lib/sanityAdmin';
 
 export default async function handler(req, res) {
 	const { authorization } = req.headers;
@@ -104,19 +19,47 @@ export default async function handler(req, res) {
 			return unauthorized();
 		}
 
+		let secondCount = 0;
+		const timeCount = setInterval(() => {
+			secondCount += 0.5;
+			console.log(secondCount);
+		}, 500);
+
 		return await createBuyer(userInfo)
 			.then(async (id) => {
+				// update item list in buyer's document
 				await updateBoughtItems(id, products, tnxId)
 					.then(async (result) => {
-						await sendProductOnEmail(products, fullname, email).then(
-							async (data) => {
-								console.log('email sent');
-								res.status(200).json({ message: 'Confirmed' });
-								return await productsAndVendors(products);
-							}
-						);
+						// await sendProductOnEmail(products, fullname, email).then(
+						// 	async (data) => {
+						// 		console.log('email sent');
+						// 		res.status(200).json({ message: 'Confirmed' });
+						// 		return await productsAndVendors(products);
+						// 	}
+						// );
 					})
 					.catch((err) => console.log('updateBoughtItem\n', err));
+
+				// send the logo to the user
+				await axios({
+					method: 'POST',
+					url: 'https://designholt.com/api/order/sendEmail',
+					data: {
+						email,
+						name: fullname,
+						items: products,
+					},
+				})
+					.then((result) => {
+						console.log('email sent');
+						res.status(200).json({ message: 'Confirmed' });
+					})
+					.catch((err) => console.log('sendEmailApi\n', err));
+
+				return (
+					clearInterval(timeCount) &&
+					console.log('Timeout ends in /api/order/create')
+				);
 			})
 			.catch((err) => console.log('createBuyer\n', err));
 	} else {
---
+++ pages/api/order/sendEmail.js
@@ -0,0 +1,111 @@
+import sgEmail from '@sendgrid/mail';
+import { productsAndVendors } from '@/lib/sanityAdmin';
+
+sgEmail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		let secondCount = 0;
+		const timeCount = setInterval(() => {
+			secondCount += 0.5;
+			console.log(secondCount);
+		}, 500);
+
+		try {
+			const { email, name, items } = req.body;
+
+			const msg = {
+				to: email,
+				from: 'funnyclub2015@gmail.com',
+				subject: 'Your download is ready',
+				// text: 'sdasdawd',
+				html: `
+          <p>Hi ${name},</p>
+          <p>Thank you for purchasing "Logo Template <a href="https://designholt.com/logo-license">(Logo Stock License)</a>" from Designholt. Read the license 
+          properly before using the "Logo Template". You can download the "Logo Template" using the "Download" button below. 
+          The product will be available on this link for the next 30 days. That's why we recommend saving the logo in secure 
+          storage.</p>
+          <a href="https://designholt.com/logo-license">Read the license here</a>
+          <br />
+          <br />
+          <table style="
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 90%;
+            margin: auto;
+          ">
+            <tr>
+              <th style="
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+              ">Product</th>
+              <th style="
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+              ">Price (USD)</th> 
+              <th style="
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+              ">Action</th>
+            </tr>
+            ${items
+							.map(
+								(item) => `
+              <tr>
+                <td style="
+                  border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+                ">${item.title}</td>
+                <td style="
+                  border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+                ">${item.price}$</td>
+                <td style="
+                  border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+                "><a href="https://designholt.com/download?productId=${
+									item.id
+								}&userId=${
+									's5d1a4sd56aw' /* userid goes here */
+								}">Download</a></td>
+              </tr>
+            `
+							)
+							.join('')}
+          </table>
+        `,
+			};
+
+			await sgEmail
+				.send(msg)
+				.then((data) => {
+					res.status(200).json({ message: 'Email sent' });
+					clearInterval(timeCount) &&
+						console.log('Timeout ends in /api/order/sendEmail');
+				})
+				.catch((err) => console.log(err));
+
+			return await productsAndVendors(items)
+				.then((result) => {
+					console.log('Products and vendors data udated');
+				})
+				.catch((err) => {
+					console.log('productsAndVendors\n', err);
+				});
+			// ------------------
+		} catch (err) {
+			console.log('sendProductEmail\n', err);
+		}
+	} else {
+		return res.status(405).send({
+			status: 'error',
+			message: 'Method not allowed',
+		});
+	}
+}
