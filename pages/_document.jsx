298a4100d05c949eaa2dfb81ffce35563a18e96a
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-03-14 00:34:21 +0600

    Bug fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,12 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin={true}
+					/>
+
 					<link
 						href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;700;800&display=swap"
 						rel="stylesheet"
