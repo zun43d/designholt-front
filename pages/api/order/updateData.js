b554faf6c36b8f294c549b712ca6e5cf16d262ee
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-04-10 03:08:20 +0600

    Evrything running well

--- pages/api/order/updateData.js
+++ pages/api/order/updateData.js
@@ -7,9 +7,11 @@ export default async function handler(req, res) {
 		return await productsAndVendors(items)
 			.then((result) => {
 				console.log('Products and vendors data updated');
+				res.status(200).json({ message: 'All documents updated!' });
 			})
 			.catch((err) => {
 				console.log('productsAndVendors\n', err);
+				res.status(500).json({ message: 'Something went wrong' });
 			});
 	} else {
 		return res.status(405).send({
