b9c5a7923e6358ebba6474f4cf14e2f9440a4188
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-07-15 03:15:18 +0600

    stripe was integrated but still something left

---
+++ components/StipeWrapper.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { Box } from '@chakra-ui/react';
+
+import StripeCheckoutForm from './StripeCheckoutForm';
+
+const stripePromise = loadStripe(
+	process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
+export default function StripeWrapper({ items }) {
+	const [clientSecret, setClientSecret] = useState('');
+
+	useEffect(() => {
+		// Create PaymentIntent as soon as the component mounts
+		fetch('/api/stripe/create-payment-intent', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ items: items }),
+		})
+			.then((res) => res.json())
+			.then((data) => setClientSecret(data.clientSecret));
+	}, [items]);
+
+	const appearance = {
+		theme: 'stripe',
+		variables: {
+			colorPrimary: '#805AD5',
+		},
+	};
+	const options = {
+		clientSecret,
+		appearance,
+	};
+
+	return (
+		<Box>
+			{clientSecret && (
+				<Elements options={options} stripe={stripePromise}>
+					<StripeCheckoutForm />
+				</Elements>
+			)}
+		</Box>
+	);
+}
